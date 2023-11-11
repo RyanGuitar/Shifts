ae5e50c79aceef4b657db8ae225887206ac7dce8
RyanGuitar <ryanelsguitar@gmail.com>
2023-11-11 13:16:42 +0200

    added service worker

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"b2128813681535c60f6c489029090ae4","url":"dropBox.js"},{"revision":"3235e419885f28443d06900ce7e4e688","url":"generateYearObject.js"},{"revision":"15b4f3e608f644ff7e4893ce2382032e","url":"helper.js"},{"revision":"b63979c566e494c01166edcb9e2fa652","url":"index.html"},{"revision":"249341abd89c76ccbfffacf8debbd16f","url":"style.css"},{"revision":"403fa0c4c264d580b55878e85fe40f87","url":"worker.js"}]);
